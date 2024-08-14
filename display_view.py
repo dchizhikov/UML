76d084a8e2bad55c31c18ebcf047501eeb4c7574
dchizhikov <dchizhikov@gmail.com>
2024-08-14 10:57:09 +0000

    display_view.py 1 commit message

---
+++ display_view.py
@@ -0,0 +1,8 @@
+#%cd $myRepo
+from display import display_scheme
+
+path = "/content/UML/orders_examples/"
+file_name="config"
+file_format="png" # ["svg", "png"]
+
+display_scheme(path, file_name, file_format="png")
