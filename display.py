48bfd90ed5c6c4abe25485a6866d1c9920f3c487
dchizhikov <dchizhikov@gmail.com>
2024-08-14 10:44:32 +0000

    display.py 2 commit message

--- display.py
+++ display.py
@@ -1,16 +1,17 @@
-#!pip3 install plantweb
+os.system("pip install plantweb")
 from IPython.display import display, Image, SVG
 
 path = "/content/UML/orders_examples/"
-os.chdir(path)
 file_name="config"
-f_in=file_name+".puml"
-
 file_format="png" # ["svg", "png"]
 
-os.system(f"plantweb --format {file_format} --no-cache {f_in}") #!plantweb --no-cache $f_in
-#https://plantweb.readthedocs.io/#command-line-interface
+def display_scheme(path, file_name, file_format="svg")
+  os.chdir(path)
+  f_in=file_name+".puml"
+  os.system(f"plantweb --format {file_format} --no-cache {f_in}") #!plantweb --no-cache $f_in
+  #https://plantweb.readthedocs.io/#command-line-interface
 
-f_out=file_name+"."+file_format
-if file_format=="svg": display(SVG(filename=f_out))
-elif file_format=="png": display(Image(filename=f_out))
+  f_out=file_name+"."+file_format
+  if file_format=="svg": scheme=SVG(filename=f_out)
+  elif file_format=="png": scheme=Image(filename=f_out)
+  display(scheme)
